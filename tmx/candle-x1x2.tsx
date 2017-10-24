5abb7ef2dd6f9e5e134c930918667e285ce03ed7
Noah Gibbs <the.codefolio.guy@gmail.com>
2017-10-24 16:25:11 -0700

    Add sprites and tilesets and AUTHORS entries for same.

--- tmx/candle-x1x2.tsx
+++ tmx/candle-x1x2.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Candle x1x2" tilewidth="32" tileheight="64" tilecount="12" columns="3">
- <image source="../tiles/candle-x1x2.png" width="96" height="256"/>
+<tileset name="Candle x1x2" tilewidth="32" tileheight="64">
+ <image source="candle-x1x2.png" width="96" height="128"/>
  <tile id="0">
   <properties>
    <property name="animation-delay0" value="13"/>
@@ -14,13 +14,6 @@
    <property name="animation-frame3" value="1"/>
    <property name="animation-frame4" value="2"/>
   </properties>
-  <animation>
-   <frame tileid="0" duration="130"/>
-   <frame tileid="1" duration="120"/>
-   <frame tileid="2" duration="130"/>
-   <frame tileid="1" duration="120"/>
-   <frame tileid="2" duration="110"/>
-  </animation>
  </tile>
  <tile id="3">
   <properties>
@@ -35,13 +28,6 @@
    <property name="animation-frame3" value="4"/>
    <property name="animation-frame4" value="5"/>
   </properties>
-  <animation>
-   <frame tileid="3" duration="130"/>
-   <frame tileid="4" duration="120"/>
-   <frame tileid="5" duration="130"/>
-   <frame tileid="4" duration="120"/>
-   <frame tileid="5" duration="110"/>
-  </animation>
  </tile>
  <tile id="6">
   <properties>
@@ -56,13 +42,6 @@
    <property name="animation-frame3" value="7"/>
    <property name="animation-frame4" value="8"/>
   </properties>
-  <animation>
-   <frame tileid="6" duration="130"/>
-   <frame tileid="7" duration="120"/>
-   <frame tileid="8" duration="130"/>
-   <frame tileid="7" duration="120"/>
-   <frame tileid="8" duration="110"/>
-  </animation>
  </tile>
  <tile id="9">
   <properties>
@@ -77,12 +56,5 @@
    <property name="animation-frame3" value="10"/>
    <property name="animation-frame4" value="11"/>
   </properties>
-  <animation>
-   <frame tileid="9" duration="130"/>
-   <frame tileid="10" duration="120"/>
-   <frame tileid="11" duration="130"/>
-   <frame tileid="10" duration="120"/>
-   <frame tileid="11" duration="110"/>
-  </animation>
  </tile>
 </tileset>
