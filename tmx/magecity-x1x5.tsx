ca55b59b40e23dd75d0a9729ab83a42a677799ea
Noah Gibbs <the.codefolio.guy@gmail.com>
2017-10-29 08:50:54 -0700

    Copy some of the magecity sprites into oversize PNGs with tilesets, then use in main map.

---
+++ tmx/magecity-x1x5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="MageCity x1x5" tilewidth="32" tileheight="160">
+ <image source="../tiles/magecity-x1x5.png" width="448" height="160"/>
+</tileset>
